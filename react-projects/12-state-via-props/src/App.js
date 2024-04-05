cddf4badf25af5f62b2410cfea8187d611b04aa4
Saverzus <saverzus@gmail.com>
2024-04-05 13:35:56 +0300

    1

---
+++ react-projects/12-state-via-props/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './components/Button.js';
+import Counter from './components/Counter.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-projects/12-state-via-props/src/components/Counter.js
@@ -0,0 +1,7 @@
+function Counter() {
+    return (
+        <h1>Cotal clicks: 0</h1>
+    )
+}
+
+export default Counter;
