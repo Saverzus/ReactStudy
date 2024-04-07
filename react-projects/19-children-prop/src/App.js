e19124e9d1eac6088dc37190056c1761e002c652
Saverzus <saverzus@gmail.com>
2024-04-07 16:53:54 +0300

    1

--- react-projects/19-children-prop/src/App.js
+++ react-projects/19-children-prop/src/App.js
@@ -4,9 +4,14 @@ import Wrapper from './components/Wrapper'
 function App() {
   return (
     <div className="App">
-      <Wrapper>
+      <Wrapper color="lightblue">
         <h2>Text inside of the Wrapper</h2>
         <button>Click me!</button>
+      </Wrapper>
+      <Wrapper color="lightgreen">
+        <h2>Another text</h2>
+        <p>Some description</p>
+        <input type="text" placeholder="Enter value"></input>
       </Wrapper>
     </div>
   );
--- react-projects/19-children-prop/src/components/Wrapper.js
+++ react-projects/19-children-prop/src/components/Wrapper.js
@@ -1,6 +1,12 @@
 function Wrapper(props) {
+    const style = {
+        backgroundColor: props.color,
+        width: '250px',
+        padding: '20px',
+        margin: '20px auto'
+    }
     return (
-        <div className="wrapper">
+        <div className="wrapper" style={style}>
             {props.children}
         </div>
     )
