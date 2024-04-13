a188bb441c0c8b4d9624d65eb43ef1a7577fdd91
Saverzus <saverzus@gmail.com>
2024-04-13 17:44:10 +0300

    UserContext.Provider

--- react-projects/27-context-api/src/App.js
+++ react-projects/27-context-api/src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+import UserContext from './context/UserContext';
+
 import './App.css';
+import User from './components/User';
+
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+  const [user, setUser] = useState('Bogdan');
+
+  return (
+    <UserContext.Provider value={user}>
+      <div className="App">
+        <User />
+      </div>
+    </UserContext.Provider >
+  )
 }
 
 export default App;
---
+++ react-projects/27-context-api/src/components/UserInfo.jsx
@@ -0,0 +1,9 @@
+import UserContext from '../context/UserContext';
+
+const UserInfo = () => {
+  return (
+    <UserContext.Consumer>{(value) => <h1>{value}</h1>}</UserContext.Consumer>
+  );
+};
+
+export default UserInfo;
