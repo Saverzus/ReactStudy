c825c2e51921cf611b3ad42c8977e2c4d0c4aab9
Saverzus <saverzus@gmail.com>
2024-04-13 18:20:57 +0300

    useContext

---
+++ react-projects/27.1-context-api-userContext/src/components/ChangeUser.jsx
@@ -0,0 +1,15 @@
+import UserContext from '../context/UserContext';
+import { useContext } from 'react';
+
+const ChangeUser = () => {
+  const { userName, changeUserName } = useContext(UserContext);
+  return (
+    <button
+      onClick={() => changeUserName(userName === 'Bogdan' ? 'Alice' : 'Bogdan')}
+    >
+      Change User
+    </button>
+  );
+};
+
+export default ChangeUser;
