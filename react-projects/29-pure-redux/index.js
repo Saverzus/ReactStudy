b0560fa2693fc88e6339af15cf12c4e1a92523c5
Saverzus <saverzus@gmail.com>
2024-04-16 23:40:16 +0300

    29-pure-redux

--- react-projects/29-pure-redux/index.js
+++ react-projects/29-pure-redux/index.js
@@ -1,14 +1,28 @@
 import store from "./redux/store.js";
-import getCurrentTime from "./utils/getCurrentTime.js";
+import { addCurrentTime, clearTimes } from "./redux/actionCreators.js";
 
-const btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-    store.dispatch({
-        type: 'ADD_CURRENT_TIME',
-        payload: getCurrentTime()
-    })
+const addTimeBtn = document.getElementById("addTime");
+addTimeBtn.addEventListener("click", () =>
+    store.dispatch(addCurrentTime())
+)
+
+const clearTimesBtn = document.getElementById("clearTimes");
+
+clearTimesBtn.addEventListener("click", () => {
+    store.dispatch(clearTimes())
 })
 
+const timesList = document.getElementById('timesList');
+
+store.subscribe(() => {
+    timesList.innerHTML = '';
+    const times = store.getState();
+    times.forEach((time) => {
+        let li = document.createElement('li');
+        li.innerText = time;
+        timesList.appendChild(li);
+    })
+})
 
 
 
--- react-projects/29-pure-redux/utils/getCurrentTime.js
+++ react-projects/29-pure-redux/utils/getCurrentTime.js
@@ -1,8 +1,8 @@
 function getCurrentTime() {
     const now = new Date();
-    const hours = now.getHours().toString();
-    const minuted = now.getMinutes().toString();
-    const seconds = now.getSeconds().toString();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minuted = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
 
     const currentTime = `${hours}:${minuted}:${seconds}`;
     return currentTime;
